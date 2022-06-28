3bdf5212a5821d5bbb9e55cbd4993fa5f02af5f8
Paul Martin <architecture@gmail.com>
2022-06-28 13:36:59 -0400

    first commit

---
+++ car_mfa.ino
@@ -0,0 +1,150 @@
+/*
+  Enabling BLE on MKR WiFi 1010
+
+  This sketch controls an LED on a MKR WiFi 1010 board
+  and makes a random reading of an analog pin.
+   
+  The data recorded can be accessed through Bluetooth,
+  using an app such as LightBlue.
+  
+  Based on the Arduino BLE library, Battery Monitor example.
+
+  (c) 2020 K. Söderby for Arduino
+*/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <ArduinoBLE.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+
+BLEService newService("180A"); // creating the service
+
+BLEUnsignedCharCharacteristic randomReading("2A58", BLERead | BLENotify); // creating the Analog Value characteristic
+BLEByteCharacteristic switchChar("2A57", BLERead | BLEWrite); // creating the LED characteristic
+
+const int ledPin = 2;
+long previousMillis = 0;
+
+
+void setup() {
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+   // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+
+  
+  Serial.begin(9600);    // initialize serial communication
+  // while (!Serial);       //starts the program if we open the serial monitor.
+  
+  pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
+  pinMode(ledPin, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
+
+  //initialize BLE library
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+    while (1);
+  }
+
+  BLE.setLocalName("MKR WiFi 1010A"); //Setting a name that will appear when scanning for bluetooth devices
+  BLE.setAdvertisedService(newService);
+
+  newService.addCharacteristic(switchChar); //add characteristics to a service
+  newService.addCharacteristic(randomReading);
+
+  BLE.addService(newService);  // adding the service
+
+  switchChar.writeValue(0); //set initial value for characteristics
+  randomReading.writeValue(0);
+
+  BLE.advertise(); //start advertising the service
+  Serial.println("Bluetooth device active, waiting for connections...");
+  showtext("Bluetooth device active, waiting for connections...");
+}
+
+void loop() {
+  
+  BLEDevice central = BLE.central(); // wait for a BLE central
+
+  if (central) {  // if a central is connected to the peripheral
+    Serial.print("Connected to central: ");
+    
+    Serial.println(central.address()); // print the central's BT address
+    showtext("Connect to central: " + central.address() + " RSSI: " + central.rssi());
+     
+    digitalWrite(LED_BUILTIN, HIGH); // turn on the LED to indicate the connection
+
+    // check the battery level every 200ms
+    // while the central is connected:
+    while (central.connected()) {
+      long currentMillis = millis();
+      
+      if (currentMillis - previousMillis >= 200) { // if 200ms have passed, we check the battery level
+        previousMillis = currentMillis;
+
+        int randomValue = analogRead(A1);
+        randomReading.writeValue((int) central.rssi()); // was randomValue
+
+        if (switchChar.written()) {
+          if (switchChar.value()) {   // any value other than 0
+            Serial.println("LED on");
+            digitalWrite(ledPin, HIGH);         // will turn the LED on
+            digitalWrite(1,HIGH);
+            showtext("Car Activated !!");
+          } else {                              // a 0 value
+            Serial.println(F("LED off"));
+            showtext("Car Disabled");
+            digitalWrite(ledPin, LOW);          // will turn the LED off
+            digitalWrite(1,LOW);
+          }
+        }
+
+      }
+    }
+    
+    digitalWrite(LED_BUILTIN, LOW); // when the central disconnects, turn off the LED
+    digitalWrite(ledPin, LOW); // turn relay off when disconnected
+    Serial.print("Disconnected from central: ");
+    
+    Serial.println(central.address());
+    showtext("Disconnected from central: " + central.address()); 
+  }
+}
+
+void showtext(String text){
+  display.clearDisplay();
+
+  display.setTextSize(1); // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 0);
+  display.println(text);
+  
+  display.display();      // Show initial text
+  delay(1000); 
+  
+}

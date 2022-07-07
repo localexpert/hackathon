c6b785f1a31213b59d716ea985522771b3d8254a
toyotaXufeng <108140010+toyotaXufeng@users.noreply.github.com>
2022-07-07 12:49:42 -0400

    Init VehicleActivator

---
+++ VehicleActivator/VehicleActivator/BLEManager.swift
@@ -0,0 +1,70 @@
+//
+//  BLEManager.swift
+//  VehicleActivator
+//
+//  Created by user222240 on 7/7/22.
+//
+
+import Foundation
+import CoreBluetooth
+
+struct Peripheral: Identifiable {
+    let id: Int
+    let name: String
+    let rssi: Int
+}
+
+class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate {
+    
+    var myCentral: CBCentralManager!
+    @Published var isSwitchedOn = false
+    @Published var peripherals = [Peripheral]()
+    
+        override init() {
+            super.init()
+     
+            myCentral = CBCentralManager(delegate: self, queue: nil)
+            myCentral.delegate = self
+        }
+
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+         if central.state == .poweredOn {
+             isSwitchedOn = true
+         }
+         else {
+             isSwitchedOn = false
+         }
+    }
+
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        var peripheralName: String!
+       
+        if let name = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
+            peripheralName = name
+        }
+        else {
+            peripheralName = "Unknown"
+        }
+       
+        let newPeripheral = Peripheral(id: peripherals.count, name: peripheralName, rssi: RSSI.intValue)
+        print(newPeripheral)
+        peripherals.append(newPeripheral)
+    }
+    
+    func startScanning() {
+         print("startScanning")
+         myCentral.scanForPeripherals(withServices: nil, options: nil)
+     }
+    
+    func stopScanning() {
+        print("stopScanning")
+        myCentral.stopScan()
+    }
+    
+    func connectWithPeripheral(peripheral: CBPeripheral) {
+        //Connect with the BLE
+        self.myCentral.connect(peripheral, options: nil)
+    }
+}
---
+++ VehicleActivator/VehicleActivator/VehicleActivatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VehicleActivatorApp.swift
+//  VehicleActivator
+//
+//  Created by user222240 on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct VehicleActivatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
